5f6bdd9284f922f1b55f2d4316859649ef56d245
yuvan-mahalingam <148525186+yuvan-mahalingam@users.noreply.github.com>
2025-02-03 12:28:40 +0530

    test Groovy

---
+++ JenkinsfileAB1.groovy
@@ -0,0 +1,45 @@
+library 'cicd-jenkins-shared-libraries@v7'
+dynamicPipeline 
+{
+    globals = [
+        CFFiles: ["cicd/ContainerDeploy.yaml",
+				  "cicd/CloudwatchAlarmsUK1.yaml"],
+        defaultLatestTag: "0.0.0",
+        ecrImageName : "wfm-etlscheduler",
+        ecrWorkstream : "integrations"    ]
+	awsProd = [
+		awsRegions : [[region: "eu-west-2", name : "London", prefix : ""]],
+        awsCredentials : "integrations_deploy_externalId",
+        awsRoleId : "pipeline-integrations-deploy-role",
+        awsCFRoleId : "pipeline-integrations-cloudformation-role",
+        awsAccountId : "737494165703"	]
+    infrastructureProd = [
+        CFTemplates :[
+            [
+				template: "cicd/CloudwatchAlarmsUK1.yaml",
+				stackName: "integrations-wfm-etlscheduler-alarms",
+				paramOverrides: [
+					'NotificationDistro="cloudwatch-bmc-info"',
+                    'NotificationDistro2="integrations-wfm-alerts"',
+                    'EnvironmentType="prod"',
+                    'Threshold=1',
+                    'Period=3600'
+				]
+			],
+            [
+                template: "cicd/ContainerDeploy.yaml",
+                stackName: "integrations-wfm-etlscheduler",
+                paramOverrides: [
+                    'Memory=256',
+                    'ContainerName="wfm-etlscheduler"',
+                    'NumberOfDockerContainers=1',
+                    'EnvironmentType="prod"',
+                    'AreaId="uk1"',
+                    'AuroraSecretName="integrations-wfm-aurora-db"',
+                    'DwSecretName="integrations-wfm-cor-db"',
+                    'AggregatorServiceHost=" "'
+                ],
+                builtDockerImageParamName : "DockerImage"
+            ]
+        ]
+    ]}
