5da3d93845b7091f0470c34361ec58dbfb0d19f5
Nareshmax <nareshmaha43@gmail.com>
2025-03-10 13:44:37 +0530

    sample_message

--- new-region-5/terraform-state/terraform.tf
+++ new-region-5/terraform-state/terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "niceincontact-terraform-state-147997157422"
-    key            = "inContact/routing-ng-infrastructure/new-region-5/terraform.tfstate"
-    region         = "new-region-5"
+    key            = "inContact/routing-ng-infrastructure/eu-west-2/terraform.tfstate" new-region-5
+    region         = "eu-west-2" new-region-5
     dynamodb_table = "niceincontact-terraform-state-locks"
   }
 }
